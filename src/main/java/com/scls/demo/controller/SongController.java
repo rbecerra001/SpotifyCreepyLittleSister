17e3755d46d7bfc5a06326e3ec90cfa682911ee9
Roger Becerra <rogelio.becerra001@gmail.com>
2021-04-12 09:38:59 -0500

    #11 set up song controller and helloworld endpoint

---
+++ src/main/java/com/scls/demo/controller/SongController.java
@@ -0,0 +1,12 @@
+package com.scls.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SongController {
+    @GetMapping("/helloworld")
+    public String helloWorld(){
+        return "Hello World";
+    }
+}
