c5658aa6cde9277fea8a1c16ffdea6a5ebda66e3
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-12 12:02:20 -0500

    #23 added exception classes

--- src/main/java/com/scls/demo/controller/LabelController.java
+++ src/main/java/com/scls/demo/controller/LabelController.java
@@ -1,5 +1,6 @@
 package com.scls.demo.controller;
 
+import com.scls.demo.service.LabelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
