04b1427cd9f51cf28ee8e0d728576e4074a54dd5
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 14:04:17 -0500

    added comments in ArtistRepository

--- src/main/java/com/scls/demo/repository/ArtistRepository.java
+++ src/main/java/com/scls/demo/repository/ArtistRepository.java
@@ -4,6 +4,12 @@ import com.scls.demo.model.Artist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+ * ArtistRepository extends the class JpaRepository thus inheriting its methods. It is an interface for the
+ * Artist entity.
+ * This class doesn't need to be implemented by the programmer because Spring will automatically implement
+ * it during runtime.
+ */
 @Repository
 public interface ArtistRepository extends JpaRepository<Artist, Long> {
 }
