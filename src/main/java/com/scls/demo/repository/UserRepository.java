a9867272627a2ea612413b8c66bda0396072e737
Roger Becerra <rogelio.becerra001@gmail.com>
2021-04-13 09:56:36 -0500

    Needed in order to pull

--- src/main/java/com/scls/demo/repository/UserRepository.java
+++ src/main/java/com/scls/demo/repository/UserRepository.java
@@ -1,8 +1,8 @@
 package com.scls.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.scls.demo.model.User;
-
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
