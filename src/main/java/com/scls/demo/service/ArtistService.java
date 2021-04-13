49c3d1af6489a8a5b16c6addc79bb7ef018681b6
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 14:23:16 -0500

    added comments in ArtistService

--- src/main/java/com/scls/demo/service/ArtistService.java
+++ src/main/java/com/scls/demo/service/ArtistService.java
@@ -13,26 +13,40 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/*
+ * ArtistService implements the business logic for ArtistController.
+ */
 @Service
 public class ArtistService {
     private ArtistRepository artistRepository;
     private LabelRepository labelRepository;
 
 
-    @Autowired
+    @Autowired // dependency injection
     public void setArtistRepository(ArtistRepository artistRepository){
         this.artistRepository = artistRepository;
     }
 
-    @Autowired
+    @Autowired // dependency injection
     public void setLabelRepository(LabelRepository labelRepository){
         this.labelRepository = labelRepository;
     }
 
+    /*
+     * Gets all artists in artist model.
+     * params: none
+     * returns: List<Artist> - a list of Artists in the model. If there are no artists, the list will be empty.
+     */
     public List<Artist> getArtists(){
         System.out.println("Calling getArtists() in service");
         return artistRepository.findAll();
     }
+
+    /*
+     * Get a single artist in artist model.
+     * params: Long artistId - id of artist
+     * returns: Artist - an Artist in the model. If the artist isn't present, an exception is thrown.
+     */
     public Artist getArtist(Long artistId){
         System.out.println("Calling getArtist() in service");
         Optional<Artist> artist = artistRepository.findById(artistId);
@@ -43,12 +57,23 @@ public class ArtistService {
         }
     }
 
+    /*
+     * Gets all the songs from a single artist.
+     * params: Long artistId - id of artist
+     * returns: List<Song> - a list of songs written by the given artist
+     */
     public List<Song> getArtistSongs(Long artistId){
         System.out.println("Calling getArtistSongs() in service");
         Artist artist = getArtist(artistId);
         return artist.getSongList();
     }
 
+    /*
+     * Get a single song from a single artist.
+     * params: Long artistId - id of artist
+     *         Long songId - id of song
+     * returns: Song - a single song written by the given artist. If the song isn't present, an exception is thrown.
+     */
     public Song getArtistSong(Long artistId, Long songId){
         System.out.println("Calling getArtistSong() in service");
         Optional<Song> song = getArtistSongs(artistId).stream().filter(s -> s.getId().equals(songId)).findFirst();
@@ -59,11 +84,22 @@ public class ArtistService {
         }
     }
 
+    /*
+     * Creates a single artist with out a Label
+     * params: Artist artistObject - information needed to create an artist (name(required), description, monthly_streamers)
+     * returns: Artist - the created Artist entry
+     */
     public Artist createArtist(Artist artistObject){
         System.out.println("Calling createArtist() in service");
         return artistRepository.save(artistObject);
     }
 
+    /*
+     * Creates a single artist with a Label
+     * params: Artist artistObject - information needed to create an artist (name(required), description, monthly_streamers)
+     *         Long labelId - the id of the label that owns the artist
+     * returns: Artist - the created Artist entry. If the label isn't present, an exception is thrown.
+     */
     public Artist createLabelArtist(Long labelId, Artist artistObject){
         System.out.println("Calling createLabelArtist() in service");
         Optional<Label> label = labelRepository.findById(labelId);
@@ -76,6 +112,12 @@ public class ArtistService {
         }
     }
 
+    /*
+     * Updates a single artist
+     * params: Artist artistObject - information needed to updated an artist (name(required), description, monthly_streamers)
+     *         Long artistId - the id of the artist to be updated
+     * returns: Artist - the updated Artist entry
+     */
     public Artist updateArtist(Long artistId, Artist artistObject){
         System.out.println("Calling updateArtist() in service");
         Artist artist = getArtist(artistId);
@@ -85,6 +127,11 @@ public class ArtistService {
         return artistRepository.save(artist);
     }
 
+    /*
+     * Deletes a single artist
+     * params: Long artistId - the id of the artist to be deleted
+     * returns: none
+     */
     public void deleteArtist(Long artistId){
         System.out.println("Calling deleteArtist() in service");
         if(artistRepository.findById(artistId).isPresent()){
