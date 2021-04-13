60d00473939c1ed42dce26f66ff88a5c82e690bf
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 14:04:51 -0500

    added comments in GenreRepository

--- src/main/java/com/scls/demo/repository/GenreRepository.java
+++ src/main/java/com/scls/demo/repository/GenreRepository.java
@@ -4,6 +4,12 @@ import com.scls.demo.model.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/*
+ * GenreRepository extends the class JpaRepository thus inheriting its methods. It is an interface for the
+ * Genre entity.
+ * This class doesn't need to be implemented by the programmer because Spring will automatically implement
+ * it during runtime.
+ */
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
