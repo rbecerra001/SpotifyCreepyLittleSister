04e0dadff2eb369aaf7db88d89c5982f43c52023
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 14:34:43 -0500

    added comments in UserService

--- src/main/java/com/scls/demo/service/UserService.java
+++ src/main/java/com/scls/demo/service/UserService.java
@@ -16,34 +16,47 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+/*
+ * UserService implements the business logic for UserController.
+ */
 @Service
 public class UserService {
     private UserRepository userRepository;
     private PasswordEncoder passwordEncoder;
-    @Autowired
+    @Autowired // dependency injection
     private AuthenticationManager authenticationManager;
-    @Autowired
+    @Autowired // dependency injection
     private UserDetailsService userDetailsService;
-    @Autowired
+    @Autowired // dependency injection
     private JWTUtils jwtUtils;
-    @Autowired
+    @Autowired // dependency injection
     public void setUserRepository(UserRepository userRepository){
         this.userRepository = userRepository;
     }
-    @Autowired
+    @Autowired // dependency injection
     public void setPasswordEncoder(PasswordEncoder passwordEncoder){
         this.passwordEncoder = passwordEncoder;
     }
 
+    /*
+     * Creates a new user
+     * params: User userObject - information needed to register a user (username(required), emailAddress(required), password(required))
+     * returns: User - the created user. Throws an exception if the user already exists.
+     */
     public User createUser(User userObject){
         if (!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
             userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
             return userRepository.save(userObject);
         }else{
-            throw  new InformationExistException("The user with " + userObject.getEmailAddress()+  " already exists.");
+            throw new InformationExistException("The user with " + userObject.getEmailAddress()+  " already exists.");
         }
     }
 
+    /*
+     * Logs a user in
+     * params: LoginRequest loginRequest - information needed for a login request (email(required), password(required))
+     * returns: ResponseEntity<?> - a response with the JWT if user was successfully logged in
+     */
     public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
         authenticationManager.authenticate(new
                 UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
@@ -52,6 +65,11 @@ public class UserService {
         return ResponseEntity.ok(new LoginResponse(JWT));
     }
 
+    /*
+     * Gets a user with their email address.
+     * Param: String email - the user's emailAddress
+     * Return: User - a user with the given email address
+     */
     public User findUserByEmailAddress(String email) {
         return userRepository.findByEmailAddress(email);
     }
