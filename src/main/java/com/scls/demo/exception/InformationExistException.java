dd06d4b5464ba8ff846d29b34ba28b8054794181
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 12:24:01 -0500

    added comments to InformationExistsException

--- src/main/java/com/scls/demo/exception/InformationExistException.java
+++ src/main/java/com/scls/demo/exception/InformationExistException.java
@@ -3,6 +3,9 @@ package com.scls.demo.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/*
+ * Creates a conflict error (409) instead of a Internal Service error (500)
+ */
 @ResponseStatus(HttpStatus.CONFLICT)
 public class InformationExistException extends RuntimeException{
     public InformationExistException(String message){
