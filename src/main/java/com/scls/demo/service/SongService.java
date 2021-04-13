febe114c0f034915e82ff6324ede5ecefba81927
Roger Becerra <rogelio.becerra001@gmail.com>
2021-04-13 11:44:44 -0500

    Commenting on code in SongService

--- src/main/java/com/scls/demo/service/SongService.java
+++ src/main/java/com/scls/demo/service/SongService.java
@@ -43,7 +43,9 @@ public class SongService {
         System.out.println("Service calling getSong ==>");
         Optional<Song> song = songRepository.findById(songId);
         if(song.isEmpty()){
+        // check is there exists a song with requested ID
             throw new InformationNotFoundException("Song with id: " + songId + "does not exist.");
+            // if no song with ID exists then an exception error is thrown
         } else {
             return song.get();
         }
@@ -53,16 +55,20 @@ public class SongService {
         System.out.println("Service calling createSong ==>");
         Optional<Artist> artist = artistRepository.findById(artistId);
         if(artist.isPresent()){
+        // check is there exists a artist with requested ID
             Optional<Genre> genre = genreRepository.findById(genreId);
             if(genre.isPresent()){
+            // check is there exists a song with requested ID
                 songObject.setArtist(artist.get());
                 songObject.setGenre(genre.get());
                 return songRepository.save(songObject);
             } else {
                 throw new InformationNotFoundException("Genre with id " + genreId + " does not exist");
+                // if no genre with ID exists then an exception error is thrown
             }
         } else {
             throw new InformationNotFoundException("Artist with id " + artistId + " does not exist");
+            // if no artist with ID exists then an exception error is thrown
         }
     }
 
@@ -70,6 +76,7 @@ public class SongService {
         System.out.println("Service calling updateSong ==>");
         Optional<Song> song = songRepository.findById(songId);
         if(song.isPresent()){
+        // check is there exists a song with requested ID
             Song updateSong = songRepository.findById(songId).get();
             updateSong.setName(songObject.getName());
             updateSong.setLength(songObject.getLength());
@@ -77,6 +84,7 @@ public class SongService {
             return songRepository.save(updateSong);
         } else {
             throw new InformationNotFoundException("Song with id " + songId + " does not exist");
+            // if no song with ID exists then an exception error is thrown
         }
     }
 
@@ -84,10 +92,12 @@ public class SongService {
         System.out.println("Service calling deleteSong ==>");
         Optional<Song> song = songRepository.findById(songId);
         if(song.isPresent()){
+        // check is there exists a song with requested ID
             songRepository.deleteById(songId);
             return song;
         } else {
             throw new InformationNotFoundException("Song with id " + songId + " does not exist");
+            // if no song with ID exists then an exception error is thrown
         }
     }
 }
