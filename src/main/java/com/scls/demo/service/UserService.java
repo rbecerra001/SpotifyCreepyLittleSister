35059a33b352d16872d6c4ab48a1cbbebbc1b5d2
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 09:35:01 -0500

    added UserService

---
+++ src/main/java/com/scls/demo/service/UserService.java
@@ -0,0 +1,59 @@
+package com.scls.demo.service;
+
+import com.scls.demo.exception.InformationExistException;
+import com.scls.demo.model.User;
+import com.scls.demo.model.request.LoginRequest;
+import com.scls.demo.model.response.LoginResponse;
+import com.scls.demo.repository.UserRepository;
+import com.scls.demo.security.JWTUtils;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JWTUtils jwtUtils;
+    @Autowired
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Autowired
+    public void setPasswordEncoder(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(User userObject){
+        if (!userRepository.existsByEmail(userObject.getEmail())){
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        }else{
+            throw  new InformationExistException("The user with " + userObject.getEmail()+  " already exists.");
+        }
+    }
+
+    public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+        authenticationManager.authenticate(new
+                UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+        final UserDetails userDetails = userDetailsService.loadUserByUsername(loginRequest.getEmail());
+        final String JWT = jwtUtils.generateToken(userDetails);
+        return ResponseEntity.ok(new LoginResponse(JWT));
+    }
+
+    public User findUserByEmailAddress(String email) {
+        return userRepository.findUserByEmail(email);
+    }
+
+}
