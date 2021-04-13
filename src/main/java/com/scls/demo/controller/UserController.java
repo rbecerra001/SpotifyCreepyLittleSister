26e2bec5bb842bc47dd6d11632a885c74a39b07c
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-13 11:41:02 -0500

    added comments to UserController

--- src/main/java/com/scls/demo/controller/UserController.java
+++ src/main/java/com/scls/demo/controller/UserController.java
@@ -10,21 +10,42 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * A Rest Controller for the User model.
+ */
 @RestController
 @RequestMapping(path = "/auth/users")
 public class UserController {
-    private UserService userService;
+    private UserService userService; // handles the business end of User Model (separation of concerns)
 
-    @Autowired
+    @Autowired // dependency injection
     public void setUserService(UserService userService){
         this.userService = userService;
     }
+
+    /*
+     * http://localhost:9092/auth/users/register
+     *
+     * Creates a new user
+     * params: User userObject - information needed to register a user (username(required), emailAddress(required), password(required))
+     * returns: User - the created user
+     */
     @PostMapping(path = "/register")
     public User createUser(@RequestBody User userObject){
+        System.out.println("Calling createUser()");
         return userService.createUser(userObject);
     }
+
+    /*
+     * http://localhost:9092/auth/users/login
+     *
+     * Logs a user in
+     * params: LoginRequest loginRequest - information needed for a login request (email(required), password(required))
+     * returns: ResponseEntity<?> - a response with the JWT if user was successfully logged in
+     */
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        System.out.println("Calling loginUser()");
         return userService.loginUser(loginRequest);
     }
 }
