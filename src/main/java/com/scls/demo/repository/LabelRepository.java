b6ce87eff692966bd9e28d47e7b7059a31825fd9
Madeleine Emmons <madeleine.emmons@protonmail.com>
2021-04-14 09:29:10 -0500

    Added user deltails to LabelService

--- src/main/java/com/scls/demo/repository/LabelRepository.java
+++ src/main/java/com/scls/demo/repository/LabelRepository.java
@@ -4,6 +4,9 @@ import com.scls.demo.model.Label;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 /*
  * LabelRepository extends the class JpaRepository thus inheriting its methods. It is an interface for the
  * Label entity.
@@ -12,4 +15,6 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface LabelRepository extends JpaRepository<Label, Long> {
+    List<Label> findByUserId(Long userId);
+    Optional<Label> findByIdAndUserId(Long labelId, Long userId);
 }
